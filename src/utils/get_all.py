6b39b83768534f0d83873090bac282216cd25167
calvin mostly <calvin@ubuntu.laptop>
2026-09-22 11:57:53 -0700

    scripted autocommit

--- src/utils/get_all.py
+++ src/utils/get_all.py
@@ -3,5 +3,5 @@ from os import system
 cmd = "curl https://www.cs.unc.edu/~kakiryan/teaching/311-sp26/slides/lecture%02d.pdf -o %s.pdf 2>/dev/null && markitdown %s.pdf -o %s.md && rm %s.pdf"
 
 for i in range(26):
-    j = "%X%X" % (i // 2, i % 2)
+    j = "%X%X" % ((i-1) // 2, (i-1) % 2)
     system(cmd % (i, j, j, j, j))
